5366284007c420f8ab02a2ef6a43567c744e02fc
waleedztp <waleedztp@gmail.com>
2024-11-09 08:32:16 +0530

    Nov 9

--- PythonATB5xLearning_24_11/StringPractice.py
+++ PythonATB5xLearning_24_11/StringPractice.py
@@ -2,3 +2,9 @@ name = input("Enter your name: ")
 print(name)
 print(name.upper())
 
+# print("Hello\nWorld") will throw error as \n is an escape sequence, in such cases use r as shown below
+print(r"Hello\nWorld")
+print("Hello\tWorld")
+print(r"Hello\tWaleed")
+print("Hello\bWorld")
+print(r"Hello\bWorld")
