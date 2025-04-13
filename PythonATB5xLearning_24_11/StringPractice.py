25fd1f6bb0dab765efe3f640563ce05a37af6a1e
waleedztp <waleedztp@gmail.com>
2025-04-13 12:38:38 +0530

    List Code Added

--- PythonATB5xLearning_24_11/StringPractice.py
+++ PythonATB5xLearning_24_11/StringPractice.py
@@ -8,3 +8,16 @@ print("Hello\tWorld")
 print(r"Hello\tWaleed")
 print("Hello\bWorld")
 print(r"Hello\bWorld")
+
+my_list = [2,4,6,8,10]
+target = 12
+is_present = False
+
+for i in range(len(my_list)):
+    for j in range(1, len(my_list)):
+        if i != j and my_list[i] + my_list[j] == target:
+            is_present = True
+            break
+    if is_present:
+        print("Combination exists..")
+        break
